df71e578be98fafac07e97a6e3806ea93eb940ef
Wagner Caetano <wcaetano@ciandt.com>
2022-10-24 19:41:00 -0300

    adicionando angular e migrando

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { FormPageComponent } from './pages/form-page/form-page.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { TutorialComponent } from './pages/tutorial/tutorial.component';
+
+const routes: Routes = [
+  { path: '**', redirectTo: '/landing' },
+  { path: 'landing', component: LandingPageComponent },
+  { path: 'tutorial', component: TutorialComponent },
+  { path: 'form', component: FormPageComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
